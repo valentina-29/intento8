4cadef45acff32aaafd36c9490f284999575df4a
valentina-29 <79235305+valentina-29@users.noreply.github.com>
2021-02-18 17:59:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioClase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0, b=0, resta=0,suma=0;
+
+            Console.WriteLine("Hola Mundo");
+            Console.ReadLine();
+
+            Console.WriteLine("primer numero");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("segundo numero");
+            b = int.Parse(Console.ReadLine());
+            resta = a - b;
+            Console.WriteLine($"la resta es: {resta}");
+            Console.ReadLine();
+
+            Console.WriteLine("primer numero");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("segundo numero");
+            b = int.Parse(Console.ReadLine());
+            suma = a + b;
+            Console.WriteLine($"la suma es: {suma}");
+
+            Console.ReadKey();
+        }
+    }
+}
